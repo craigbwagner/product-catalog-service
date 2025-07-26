6a624161938b19fbf853b560f5f41d6340d9231c
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-26 19:50:12 -0400

    refactor to just include one List function with placeholder products

--- internal/product/handler.go
+++ internal/product/handler.go
@@ -5,26 +5,7 @@ import (
 	"net/http"
 )
 
-func Handler() http.Handler {
-	mux :=http.NewServeMux()
-
-	mux.HandleFunc("/health", handleHealth)
-	mux.HandleFunc("/products", handleProducts)
-
-	return mux
-}
-
-func handleHealth(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
-}
-
-func handleProducts(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		products := []string{"laptop", "phone", "tablet"}
-		json.NewEncoder(w).Encode(products)
-
-		return
-	}
-
-	http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+func List(w http.ResponseWriter, r *http.Request) {
+	products := []string{"laptop", "phone", "tablet"}
+	json.NewEncoder(w).Encode(products)
 }
