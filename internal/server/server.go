be2ea90d95403dcf2c01e2c7e2d1a7f82ebfadf0
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-26 19:30:33 -0400

    add Start function for server initialization

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,13 @@
 package server
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Start(r *chi.Mux) {
+	log.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
