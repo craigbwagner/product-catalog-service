54cfda84dec99ff657f7bc69a2c63f541a8461d2
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-26 19:30:19 -0400

    add Router function for establishing routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,1 +1,20 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func Router() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world!"))
+	})
+
+	return r
+}
